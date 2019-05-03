cb2d36f05a72b60453f39923f4f76c09b0d8084f
Hema86 <hemakempur@gmail.com>
2019-05-03 22:18:05 +0530

    added class component

--- src/App.js
+++ src/App.js
@@ -1,65 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
+class Memorycard extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { isToggleOn: true }
+  }
+
   render () {
     return (
-      <div className='Memory-game'>
-        <div class='memory-card' data-framework='aurelia'>
-          {/* <img class='front-face' src='../img/aurelia.svg' alt='Aurelia' /> */}
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-        <div class='memory-card' data-framework='aurelia'>
-          <img class='front-face' src='./img/aurelia.svg' alt='Aurelia' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-
-        <div class='memory-card' data-framework='vue'>
-          <img class='front-face' src='./img/vue.svg' alt='Vue' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-        <div class='memory-card' data-framework='vue'>
-          <img class='front-face' src='./img/vue.svg' alt='Vue' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-
-        <div class='memory-card' data-framework='angular'>
-          <img class='front-face' src='./img/angular.svg' alt='Angular' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-        <div class='memory-card' data-framework='angular'>
-          <img class='front-face' src='./img/angular.svg' alt='Angular' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-
-        <div class='memory-card' data-framework='ember'>
-          <img class='front-face' src='./img/ember.svg' alt='Ember' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-        <div class='memory-card' data-framework='ember'>
-          <img class='front-face' src='./img/ember.svg' alt='Ember' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-
-        <div class='memory-card' data-framework='backbone'>
-          <img class='front-face' src='./img/backbone.svg' alt='Backbone' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-        <div class='memory-card' data-framework='backbone'>
-          <img class='front-face' src='./img/backbone.svg' alt='Backbone' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-
-        <div class='memory-card' data-framework='react'>
-          <img class='front-face' src='./img/react.svg' alt='React' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
-        <div class='memory-card' data-framework='react'>
-          <img class='front-face' src='./img/react.svg' alt='React' />
-          <img class='back-face' src='../img/js-badge.svg' alt='JS Badge' />
-        </div>
+      <div onClick={this.handleClick} class={`memory-card ${this.state.isToggleOn ? 'flip' : ''}`}>
+        <img class='back-face' src={this.props.img.back} alt='back' />
+        <img class='front-face' src={this.props.img.front} alt='front' />
       </div>
     )
   }
 }
 
-export default App
+export default Memorycard
--- src/index.js
+++ src/index.js
@@ -1,57 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './App'
+import Memorycard from './App'
 import './index.css'
 
-function App () {
-  let state = {
-    images: [
-      {
-        front: '../img/js-badge.svg',
-        back: '../img/aurelia.svg'
-      },
-      {
-        front: '../img/js-badge.svg',
-        back: '../img/vue.svg'
-      },
-      {
-        front: '../img/js-badge.svg',
-        back: '../img/angular.svg'
-      },
-      {
-        front: '../img/js-badge.svg',
-        back: '../img/ember.svg'
-      },
-      {
-        front: '../img/js-badge.svg',
-        back: '../img/backbone.svg'
-      },
-      {
-        front: '../img/js-badge.svg',
-        back: '../img/react.svg'
-      }
-    ],
-    activeItems: [],
-    matchedItems: []
+// function App () {
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      images: [
+        {
+          front: '../img/js-badge.svg',
+          back: '../img/aurelia.svg'
+        },
+        {
+          front: '../img/js-badge.svg',
+          back: '../img/vue.svg'
+        },
+        {
+          front: '../img/js-badge.svg',
+          back: '../img/angular.svg'
+        },
+        {
+          front: '../img/js-badge.svg',
+          back: '../img/ember.svg'
+        },
+        {
+          front: '../img/js-badge.svg',
+          back: '../img/backbone.svg'
+        },
+        {
+          front: '../img/js-badge.svg',
+          back: '../img/react.svg'
+        }
+      ],
+      activeItems: [],
+      matchedItems: []
+    }
   }
-
-  const card = state.images.map((image) =>
-    <Memorygame img={image} />)
-  return (
-    <div className='Memory-game'>
-      {card}
-      {card}
-    </div>
-  )
-}
-
-function Memorygame (props) {
-  return (
-    <div class='memory-card'>
-      <img class='back-face' />
-      <img class={`front-face ${props.active ? 'flip' : ''}`} src={props.img.front} />
-    </div>
-  )
+  render () {
+    const card = this.state.images.map((image) =>
+      <Memorycard img={image} />)
+    return (
+      <div className='Memory-game'>
+        {card}
+        {card}
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
