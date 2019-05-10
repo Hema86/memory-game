abcfb6c07b4bd98b8c99cfe344796a337aeb9456
Hema86 <hemakempur@gmail.com>
2019-05-10 11:14:52 +0530

    refactered the code

--- src/index.js
+++ src/index.js
@@ -8,27 +8,27 @@ class App extends React.Component {
     this.state = {
       items: [
         {
-          'id': 1,
+          id: 1,
           img: '../img/ember.svg'
         },
         {
-          'id': 2,
+          id: 2,
           img: '../img/angular.svg'
         },
         {
-          'id': 3,
+          id: 3,
           img: '../img/react.svg'
         },
         {
-          'id': 4,
+          id: 4,
           img: '../img/backbone.svg'
         },
         {
-          'id': 5,
+          id: 5,
           img: '../img/vue.svg'
         },
         {
-          'id': 6,
+          id: 6,
           img: '../img/aurelia.svg'
         }
       ],
@@ -39,21 +39,30 @@ class App extends React.Component {
   }
 
   handleClick (index) {
+    // console.log(index)
     let { activeItems, matchedItems, items } = this.state
+    console.log('///' + activeItems)
     if (activeItems.length >= 2) {
       activeItems.length = 0
       activeItems.push(index)
     } else {
       activeItems.push(index)
+      // console.log(activeItems)
       if (activeItems.length === 2) {
         if (items[activeItems[0]].id === items[activeItems[1]].id) {
           matchedItems.push(items[activeItems[0]].id)
+          // console.log(matchedItems)
         }
       }
     }
     this.setState({
       activeItems
     })
+    // console.log(activeItems)
+    // console.log(matchedItems)
+    console.log('act' + activeItems)
+    console.log('matched' + matchedItems)
+    console.log('----------------------------')
   }
 
   componentWillMount () {
@@ -68,7 +77,10 @@ class App extends React.Component {
     return (
       <div className='Memory-game'>
         {items.map((item, i) => {
+          // console.log(activeItems.indexOf(i))
+          // console.log(matchedItems.indexOf(i))
           return <Card
+            key={i}
             index={i}
             item={item}
             active={activeItems.indexOf(i) !== -1}
@@ -88,7 +100,7 @@ const Card = (props) => {
         <div className='front'>
           <img src='../img/js-badge.svg' alt='front' />
         </div>
-        <div className='back' onClick={(e) => { e.stopPropagation() }}>
+        <div className='back' >
           <img src={props.item.img} alt='back' />
         </div>
       </div>
