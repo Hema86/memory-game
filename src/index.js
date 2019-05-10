1e6d0ed879e272372b7232d348685841771db975
Hema86 <hemakempur@gmail.com>
2019-05-10 06:13:05 +0530

    added flipping

--- src/index.js
+++ src/index.js
@@ -1,83 +1,100 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Memorycard from './App'
-// import _ from 'lodash.hasin'
-import './index.css'
+import '../src/index.css'
 
-// function App () {
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      images: [{
-        id: 1,
-        name: '../img/aurelia.svg'
-      },
-      {
-        id: 1,
-        name: '../img/vue.svg'
-      },
-      {
-        id: 1,
-        name: '../img/angular.svg'
-
-      },
-      {
-        id: 1,
-        name: '../img/ember.svg'
-      },
-      {
-        id: 1,
-        name: '../img/backbone.svg'
-
-      },
-      {
-        id: 1,
-        name: '../img/react.svg'
-
-      }],
+      items: [
+        {
+          'id': 1,
+          img: '../img/ember.svg'
+        },
+        {
+          'id': 2,
+          img: '../img/angular.svg'
+        },
+        {
+          'id': 3,
+          img: '../img/react.svg'
+        },
+        {
+          'id': 4,
+          img: '../img/backbone.svg'
+        },
+        {
+          'id': 5,
+          img: '../img/vue.svg'
+        },
+        {
+          'id': 6,
+          img: '../img/aurelia.svg'
+        }
+      ],
       activeItems: [],
       matchedItems: []
     }
-    // this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
+
+  handleClick (index) {
+    let { activeItems, matchedItems, items } = this.state
+    if (activeItems.length >= 2) {
+      activeItems.length = 0
+      activeItems.push(index)
+    } else {
+      activeItems.push(index)
+      if (activeItems.length === 2) {
+        if (items[activeItems[0]].id === items[activeItems[1]].id) {
+          matchedItems.push(items[activeItems[0]].id)
+        }
+      }
+    }
+    this.setState({
+      activeItems
+    })
+  }
+
+  componentWillMount () {
+    var { items } = this.state
+    this.setState({
+      items: items.concat(items.slice(0))
+    })
+  }
+
   render () {
-    const card = this.state.images.map((image, i) =>
-      <Memorycard item={image} />)
+    var { activeItems, matchedItems, items } = this.state
     return (
       <div className='Memory-game'>
-        {card}
-        {card}
+        {items.map((item, i) => {
+          return <Card
+            index={i}
+            item={item}
+            active={activeItems.indexOf(i) !== -1}
+            matched={matchedItems.indexOf(item.id) !== -1}
+            handleClick={this.handleClick} />
+        })}
       </div>
     )
   }
 }
 
-class Memorycard extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { isToggleOn: false }
+const Card = (props) => {
+  return (
 
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick (e) {
-    // console.log(e.target)
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }))
-  }
-  render () {
-    // console.log(this.props).item
-    return (
-      <div onClick={this.handleClick} className='memory-card'>
-        {this.state.isToggleOn
-          ? <img className='back-face' src={this.props.item.name} alt='back' />
-          // activeItems.push(this.props.item.name, indexOf('this.props.item.name'))
-          : <img className='front-face' src='../img/js-badge.svg' alt='front' />
-        }
+    <div className={`card ${props.active || props.matched ? 'active' : ''}`} onClick={props.handleClick.bind(this, props.index)}>
+      <div className='flipper'>
+        <div className='front'>
+          <img src='../img/js-badge.svg' alt='front' />
+        </div>
+        <div className='back' onClick={(e) => { e.stopPropagation() }}>
+          <img src={props.item.img} alt='back' />
+        </div>
       </div>
-    )
-  }
+    </div>
+
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
